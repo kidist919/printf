bea6aa2a76d28dd3f152aa4be36b4e2c0d2b1cbc
kidist919 <99682094+kidist919@users.noreply.github.com>
2022-04-16 12:36:41 -0700

    putchar fix

---
+++ _puts.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints a string
+ * new line, to stdout.
+ * @string: input string
+ * Return: count of string.
+ */
+int _puts(char *string)
+{
+	int contador = 0;
+
+	while (*string)
+	{
+		_putchar(*string);
+		string++;
+		contador++;
+	}
+	return (contador);
+}
+© 2022 GitHub, Inc.
